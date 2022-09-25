4b0e709ebeb55ba22e7b1d39ba2b68c98a5f501c
PraveenAkashk <praveenkadhiravan52853@gmail.com>
2022-09-25 18:27:15 +0530

    script added

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+let menubutton=document.getElementById("menuicon");
+let navbar=document.getElementById("navbar");
+let touchbutton=document.getElementById("touchbtn");
+let sectext= document.getElementById("sectxt")
+
+touchbutton.addEventListener('click', ()=>{
+   console.log("clicked");
+})
+
+
