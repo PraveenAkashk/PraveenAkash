b2d6eadc03deceec14d83951a02930831c9be882
PraveenAkashk <praveenkadhiravan52853@gmail.com>
2022-10-16 20:48:08 +0530

    script added seperately

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
+let menubutton = document.getElementById("menuicon");
+let navbar = document.getElementById("navbar");
+let touchbutton = document.getElementById("touchbtn");
+let sectext = document.getElementById("sectxt");
 
-
-let menubutton=document.getElementById("menuicon");
-let navbar=document.getElementById("navbar");
-let touchbutton=document.getElementById("touchbtn");
-let sectext= document.getElementById("sectxt")
-
-touchbutton.addEventListener('click', ()=>{
-   console.log("clicked");
-})
-
-
+menubutton.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+});
