431c349d5253ecf42c5178ed617be6decd6c650e
Praveen Akash Kadhiravan <Praveenakashk@ducontindia.local>
2025-01-01 09:07:20 +0530

    year updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import Siteheader from './components/Siteheader'
-import { Themecontext, ThemeProvider } from "./context/Themecontext"
+import Layout from './components/Layout'
+import { ThemeProvider } from "./context/Themecontext"
 import "./assets/stylesheets/components/Responsive.scss"
 import './App.scss'
 import "./assets/stylesheets/components/theme/Theme.scss"
-import Layout from './components/Layout'
-import { useContext, useState } from 'react'
 
 function App() {
 
   return (
     <>
-    <ThemeProvider>
-      <Siteheader/>
-      <Layout  />
-    </ThemeProvider>
+      <ThemeProvider>
+        <Siteheader />
+        <Layout />
+      </ThemeProvider>
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,13 @@ import { Container } from "react-bootstrap";
 import "../assets/stylesheets/components/Footer.scss"
 
 let Footer = () => {
-
+    let getYear = new Date().getFullYear()
+    
     return (
         <div className="footer-main">
             <Container>
                 <div className="footer-wrap text-center">
-                    &copy; 2024 All rights reserved.
+                    &copy; {getYear} All rights reserved.
                 </div>
             </Container>
         </div>
